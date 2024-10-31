4bf162ae214190f5e7da3893271be802aa623e16
timolai-andrievich <timolai.andrievitch@ya.ru>
2024-10-31 18:55:44 +0300

    Updated compilation script and added links

--- compile.sh
+++ compile.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 pdflatex --halt-on-error trd.tex
+pdflatex --halt-on-error trd.tex # Recompile to include the table of contents
 
 COMPILED=$?
 if [ $COMPILED -eq 0 ]; then 
-  rm *.toc
+  rm *.toc 
   rm *.log
   rm *.aux
 fi
